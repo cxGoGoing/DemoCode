f79d32be71e7753a05e4727ee28945567e26d440
chengxun <chengxun@xiaoshouyi.com>
2016-05-23 16:56:50 +0800

    初始化一个图层视图

--- Charts/Controllers/HomeViewController.m
+++ Charts/Controllers/HomeViewController.m
@@ -7,9 +7,9 @@
 //
 
 #import "HomeViewController.h"
-
+#import <PureLayout.h>
 @interface HomeViewController ()
-
+@property (nonatomic,strong)UIImageView * backImageView;
 @end
 
 @implementation HomeViewController
@@ -18,7 +18,34 @@
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor whiteColor];
     DDLogInfo(@"12123123");
+    [self setUpUI];
     // Do any additional setup after loading the view.
+}
+- (UIImageView*)backImageView{
+    if(!_backImageView){
+        _backImageView = [[UIImageView alloc]init];
+        _backImageView.backgroundColor = [UIColor grayColor];
+        [self.view addSubview:_backImageView];
+    }
+    return _backImageView;
+}
+
+- (void)setUpUI{
+    [self.backImageView autoSetDimensionsToSize:CGSizeMake(200, 200)];
+    [self.backImageView autoCenterInSuperview];
+    CALayer * layer = [CALayer layer];
+    layer.anchorPoint = CGPointMake(0.5, 1);
+    layer.position = CGPointMake(15, 200);
+    layer.bounds = CGRectMake(0, 0, 30, 150);
+    layer.backgroundColor = [UIColor redColor].CGColor;
+    CABasicAnimation * animation = [CABasicAnimation animation];
+    animation.keyPath = @"transform.scale.y";
+    animation.toValue = @0.1;
+    animation.repeatCount = MAXFLOAT;
+    animation.autoreverses = YES;
+    animation.duration = 0.5;
+    [layer addAnimation:animation forKey:@"scaleAnimation"];
+    [self.backImageView.layer addSublayer:layer];
 }
 
 - (void)didReceiveMemoryWarning {
